60112f8fbdad660c7a989a68c4aae688385619a8
RomanLeonenko <lrp-msk@yandex.ru>
2021-11-17 19:06:14 +0300

    Secondary commit

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,25 +1,21 @@
 public class Variable {
-
     public static void main(String[] args) {
 
         byte core = 6;
         short streams = 12;
         long ram = 16;
-        float ramspeed = 3.0f;
-        double processorfrequency = 4.4;
+        float ramSpeed = 3.0f;
+        double processorFrequency = 4.4;
         char ssd = 'K';
         boolean zalman = true;
-        String oc = "Windows 10 Pro";
-        String processor = "AMD Ryzen 5 3600X";
 
         System.out.println(core);
         System.out.println(streams);
         System.out.println(ram);
-        System.out.println(ramspeed);
-        System.out.println(processorfrequency);
+        System.out.println(ramSpeed);
+        System.out.println(processorFrequency);
         System.out.println(ssd);
         System.out.println(zalman);
-        System.out.println(oc);
-        System.out.println(processor);
+
     }
 }
