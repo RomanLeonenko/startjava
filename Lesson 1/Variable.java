78a3caaa3b3f62b09fe9f30a9e16ba34a158208d
RomanLeonenko <lrp-msk@yandex.ru>
2021-11-18 13:45:30 +0300

    work on errors2

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,12 +1,11 @@
 public class Variable {
     public static void main(String[] args) {
-
         byte core = 6;
         short streams = 12;
         int powerSupplyUnit = 600;
         long ram = 16;
         float ramSpeed = 3.0f;
-        double processorFrequency = 4.4;
+        double cpu = 4.4;
         char ssd = 'K';
         boolean zalman = true;
 
@@ -15,7 +14,7 @@ public class Variable {
         System.out.println(powerSupplyUnit);
         System.out.println(ram);
         System.out.println(ramSpeed);
-        System.out.println(processorFrequency);
+        System.out.println(cpu);
         System.out.println(ssd);
         System.out.println(zalman);
     }
